80b9fd6c6d1d366edaa2175d2f573466dd108c21
denis ukrajev <humanwithbeard@gmail.com>
2023-08-24 20:44:56 +0300

    second func added+docstring

--- str_func.py
+++ str_func.py
@@ -1,6 +1,15 @@
 def capitalize_all(input_string):
+    """"docstring"""
     return input_string.upper()
 
 input_str = input("Введите строку: ")
 result = capitalize_all(input_str)
 print(result)
+
+def capitalize_words(input_string):
+    """"docstring"""
+    return input_string.title()
+
+input_str = input("Введите строку: ")
+capitalized_result = capitalize_words(input_str)
+print(capitalized_result)
